8c07e49f74c8e011e00329c789a5f476e51ead5b
TheOldOwl <alex.aluchi@gmail.com>
2018-03-01 17:21:57 +0200

    backend connection updates

--- src/App.js
+++ src/App.js
@@ -92,7 +92,7 @@ class App extends Component {
   onSubmit = () => {
     console.log('click')
     this.setState({imageUrl: this.state.input})
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://morning-castle-69645.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -102,7 +102,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://morning-castle-69645.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
